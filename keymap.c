3c793537eaebbbd96d4cdaffa5ca517a56468e75
Micah Loffer <micah.loffer@gmail.com>
2017-11-16 15:55:20 -0800

    Add config files to enable tap dance (not yet implemented)

--- keymap.c
+++ keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_A,    KC_S,    KC_H,    KC_T,    KC_G,           KC_Y,    KC_N,    KC_E,    KC_O,    KC_I    },
   { KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,           KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH },
   { XXXXXXX, KC_HOME, KC_LGUI, KC_TAB,  XXXXXXX,        KC_ESC, KC_BSPC,  KC_DEL,  KC_PGUP, XXXXXXX },
-  { XXXXXXX, KC_END,  KC_LCTL, KC_ENT,  FNKEY,          SHIFT,   KC_SPC,  KC_LALT, KC_PGDN, XXXXXXX }
+  { XXXXXXX, KC_END,  KC_LCTL, KC_ENT,  FNKEY,          TD(TD_SHIFT_CAPS),   KC_SPC,  KC_LALT, KC_PGDN, XXXXXXX }
 },
 
 
@@ -181,9 +181,9 @@ void matrix_scan_user(void) {
 };
 
 // Currently not working
-//Tap Dance Definitions
-// qk_tap_dance_action_t tap_dance_actions[] = {
-//   //Tap once for shift, twice for Caps Lock
-//   [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(SHIFT, KC_CAPS)
-// // Other declarations would go here, separated by commas, if you have them
-// };
+// Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for shift, twice for Caps Lock
+  [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
+// Other declarations would go here, separated by commas, if you have them
+};
